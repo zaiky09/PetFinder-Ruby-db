4f3b6fcc4f3eff83ab2286d8c5c6455445f3e00c
zaiky09 <zamilmozamil@outlook.com>
2023-03-05 12:43:44 +0300

    Project initialised and user_controller set up

---
+++ Gemfile
@@ -0,0 +1,25 @@
+source "https://rubygems.org/"
+
+gem 'sinatra', '~> 3.0', '>= 3.0.5'
+
+gem 'sinatra-cross_origin', '~> 0.4.0'
+
+gem 'activerecord', '~> 7.0', '>= 7.0.4.2'
+
+gem 'rake', '~> 13.0', '>= 13.0.6'
+
+gem 'sqlite3'
+
+gem 'bcrypt', '~> 3.1', '>= 3.1.18'
+
+gem 'sinatra-activerecord'
+
+gem 'puma', '~> 6.1'
+
+gem 'require_all'
+
+group :development do 
+    gem 'rerun', '~> 0.14.0'
+    gem 'faker'
+    gem 'pry'
+end
---
+++ app/controllers/user_control.rb
@@ -0,0 +1,55 @@
+class UserController < AppController
+    # @helper: JSON
+    before do
+        begin
+          @user = user_data
+        rescue
+          @user = nil
+        end
+    end
+
+
+   #@method: new user
+   post '/auth/signup' do
+    begin
+      user = User.create(@user)
+      unless user.save
+        puts " errors: #{user.errors.full_messages}"
+      end
+      json_response(code: 201, data: user)
+    rescue => e
+      error_response(422, e)
+    end
+   end
+     
+    #@method
+  post '/auth/login' do
+    begin
+      user_data = User.find_by(email: @user['email'])
+      if user_data.password == @user['password']
+        json_response(code: 200, data: {
+          id: user_data.id,
+          email: user_data.email
+        })
+      else
+        json_response(code: 420, data: { message: "Your email/password combination is not correct" })
+      end
+    rescue => e
+      error_response(420, e)
+    end
+  end
+     
+  private
+
+  # @helper
+  def user_data
+    json_data = JSON.parse(request.body.read)
+    puts "JSON data received: #{json_data}"
+    json_data
+    rescue JSON::ParserError => e
+     puts "Failed to parse JSON data: #{e}"
+      nil
+   end
+
+
+end
