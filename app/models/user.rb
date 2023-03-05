57b9050c24d8aa3c011fd06ae33c056f8012b9be
zaiky09 <zamilmozamil@outlook.com>
2023-03-05 12:48:24 +0300

    database.yml done

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+class User < ActiveRecord::Base 
+    has_many :
+
+    include BCrypt
+    
+    #retrieve password from hash
+    def password
+        @password ||= Password.new(password_hash)
+    end
+    
+      # create the password hash
+    def password=(new_pass)
+        @password = Password.create(new_pass)
+        self.password_hash = @password
+    end
+
+    def full_name(user)
+        "#{user.firstname} #{user.lastname}"
+    end
+
+end
